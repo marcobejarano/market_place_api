df3277850df24f868b824cb16c9fe5bb79d703e9
Marco <m_uno_p@hotmail.com>
2024-04-01 18:17:09 -0500

    Adds search scopes on the product model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
-  has_secure_password
+  
   has_many :products, dependent: :destroy
+
+  has_secure_password
 end
