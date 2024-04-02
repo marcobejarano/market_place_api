e264530424a2f35d61b32a3cfe8f34f779deef9c
Marco <m_uno_p@hotmail.com>
2024-04-01 20:35:26 -0500

    Associates products and orders with a placements model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to :user
+  has_many :placements, dependent: :destroy
+  has_many :products, through: :placements
+
   validates :total, numericality: {
     greater_than_or_equal_to: 0
   }, presence: true
-
-  belongs_to :user
 end
