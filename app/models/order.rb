74a5294ef16cb9dbf43ac0e9e69227b669389512
Marco <m_uno_p@hotmail.com>
2024-04-01 20:07:41 -0500

    Generate orders

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ApplicationRecord
+  validates :total, numericality: {
+    greater_than_or_equal_to: 0
+  }, presence: true
+
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_31_184819) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_005532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "total"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -31,5 +39,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_31_184819) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end
